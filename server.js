3a9214f3f6c31e716141c43ccf62dd673d04f2db
Innokentie <69416693+Innokentie@users.noreply.github.com>
2022-02-12 16:53:18 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
-const express = require('express');
+const express = require("express");
 const createServer = require("http");
 const Server = require("socket.io");
+const fs = require("fs");
+
+/*
+function RestDB(){
+    fs.readFile('db.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        var messages = data;
+        console.log(`Read done: ${messages}`);
+
+        fs.writeFile(
+            'db.txt',
+            `${messages} NEW`,
+            'utf8',
+            (err) => {
+            if (err) throw err;
+
+            console.log('Writing done!');
+            }
+        );
+    });
+};
+
+RestDB();
+*/
+
 
 const app = express();
 const httpServer = createServer.createServer(app);
@@ -10,7 +35,7 @@ const io = new Server.Server(httpServer, {});
 // Variables
 var chatStats = new Object();
 var chatStats = {
-    users_online: 0
+    users_online: 0,
 };
 
 
@@ -25,21 +50,79 @@ io.on("connection", (socket) => {
     // New user
     console.log(`Connected ${socket.id}!`);
     chatStats.users_online += 1;
-    io.emit("new_user", {id: socket.id, users: chatStats.users_online});
+
+    // Write db
+    fs.readFile('db.txt', 'utf8', (err, mdata) => {
+        if (err) throw err;
+
+        var nmessage = `${mdata}<br>\n<strong>Server:</strong> Подключается новый пользователь <i>%USERNAME%</i> | ID: ${socket.id}<br>`;
+        fs.writeFile(
+            'db.txt',
+            nmessage,
+            'utf8',
+            (err) => {
+            if (err) throw err;
+            }
+        );
+        socket.emit("new_user_socket", {messages: mdata});
+        io.emit("new_user", {id: socket.id, users: chatStats.users_online});
+    });
+
+
 
     // Exit user
     socket.on("disconnect", (data) => {
         console.log(socket.id, "disconnected");
         chatStats.users_online -= 1;
         io.emit("udisconnect", {id: socket.id, users: chatStats.users_online})
+
+        // Write db
+        fs.readFile('db.txt', 'utf8', (err, mdata) => {
+            if (err) throw err;
+
+            var nmessage = `${mdata}<br>\n<strong>Server:</strong> <i>${socket.id}</i> Вышел из чата<br>`;
+            fs.writeFile(
+                'db.txt',
+                nmessage,
+                'utf8',
+                (err) => {
+                if (err) throw err;
+                }
+            );
+        });
+
     });
+
+
 
     // New message
     socket.on("new_message", (data) => {
         console.log(`${data.username}: ${data.message}`)
         io.emit("new_message", {username: data.username, message: data.message});
+
+        // Write db
+        fs.readFile('db.txt', 'utf8', (err, mdata) => {
+            if (err) throw err;
+
+            var nmessage = `${mdata}<br>\n<strong>${data.username}</strong>: ${data.message}<br>\n`;
+            fs.writeFile(
+                'db.txt',
+                nmessage,
+                'utf8',
+                (err) => {
+                if (err) throw err;
+                }
+            );
+        });
     });
+
 });
 
-var port = process.env.PORT || 8000;
-httpServer.listen(port, () => console.log('Server ready!'));
+
+/*
+1. БД сообщений и пользователей
+2. Настройка: цвет ника, цветовая тема, автоскролл
+*/
+
+
+httpServer.listen(3000, () => console.log('Server ready!'));
